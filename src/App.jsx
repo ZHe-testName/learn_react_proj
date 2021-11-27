47246553b5e0250624143f34ecc7fa81e517d150
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2021-11-27 16:02:32 +0200

    Added sortinf of the posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,22 @@ import PostsList from "./components/PostsList";
 
 import './App.css';
 import AddPostForm from "./components/AddPostForm";
+import MySelect from "./UI/my_select/MySelect";
 
 const initialPostsState = [
-    {id: '1', title: 'JavaScript', description: 'JS'},
-    {id: '2', title: 'TypeScript', description: 'TS'},
-    {id: '3', title: 'Phyton', description: 'PY'},
+    {id: '1', title: 'JS', description: 'jjj'},
+    {id: '2', title: 'TypeScript', description: 'mnnmm'},
+    {id: '3', title: 'Phyton', description: 'ppoui'},
+];
+
+const optionsArr = [
+    {title: 'title', value: 'title'},
+    {title: 'description', value: 'description'},
 ];
 
 function App (){
     const [postsArr, setPostsArr] = useState(initialPostsState);
+    const [sortingValue, setSortingValue] = useState('');
 
     function addPost (post){
         const newPost = {
@@ -25,8 +32,13 @@ function App (){
     };
 
     function deletePost (id){
-        console.log(id);
         setPostsArr(postsArr.filter(post => post.id !== id));
+    };
+
+    function sortPosts (value){
+        setSortingValue(value);
+
+        setPostsArr([...postsArr].sort((a, b) => a[value].localeCompare(b[value])));
     };
 
     return (
@@ -37,7 +49,19 @@ function App (){
 
             <AddPostForm addPost={addPost}/>
 
-            <PostsList posts={postsArr} deletePost={deletePost}/>
+            <hr style={{backgroundColor: 'teal', margin: '10px 0px'}}/>
+
+            <MySelect
+                value={sortingValue} 
+                options={optionsArr} 
+                defaultValue='Sort on...'
+                onChange={sortPosts}/>
+
+            {
+                (postsArr.length)
+                    ? <PostsList posts={postsArr} deletePost={deletePost}/>
+                    : <h1 className='h1_not_found'>Posts are not found</h1>
+            }
         </div>
     );
 };
